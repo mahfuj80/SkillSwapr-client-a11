fdd7126f2323f757c1c602041436f0ed41dca6fa
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-06 19:17:59 +0600

    reRender related problem fix in react table

--- src/Pages/Home/Jobs/Jobs.jsx
+++ src/Pages/Home/Jobs/Jobs.jsx
@@ -1,36 +1,46 @@
-import axios from 'axios';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import { useQuery } from '@tanstack/react-query';
+// import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Jobs = () => {
-  // const [allJobs, setAllJobs] = useState([]);
+  const axios = useAxiosSecure();
+  const [allJobs, setAllJobs] = useState({});
+  const [category, setCategory] = useState('');
+
+  const getJobs = async () => {
+    const res = await axios.get(`/jobs?category=${category}`, {
+      withCredentials: true,
+    });
+    // console.log(res.data);
+    setAllJobs(res);
+  };
 
   //   get all jobs by filter and conditions
-  const {
-    isPending,
-    isError,
-    data: allJobs,
-    error,
-  } = useQuery({
-    queryKey: ['todos'],
-    queryFn: () => axios.get('http://localhost:5000/jobs'),
-  });
+  // const {
+  //   isLoading,
+  //   isError,
+  //   data: allJobs,
+  //   error,
+  // } = useQuery({
+  //   queryKey: ['jobs', category],
+  //   queryFn: getJobs,
+  // });
 
-  if (isPending) {
-    return <span>Loading...</span>;
-  }
+  // if (isLoading) {
+  //   return <span>Loading...</span>;
+  // }
 
-  if (isError) {
-    return <span>Error: {error.message}</span>;
-  }
+  // if (isError) {
+  //   return <span>Error: {error.message}</span>;
+  // }
 
-  // useEffect(() => {
-  //   axios
-  //     .get('http://localhost:5000/jobs')
-  //     .then((res) => setAllJobs(res?.data));
-  // }, []);
-  console.log(allJobs[0]);
+  useEffect(() => {
+    getJobs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category]);
   return (
     <section className="pt-20 lg:pt-[50px] lg:pb-[90px] px-8 md:px-14">
       <div className="-mx-4 flex flex-wrap">
@@ -51,40 +61,121 @@ const Jobs = () => {
       </div>
       <Tabs>
         <TabList>
-          <Tab>All Jobs</Tab>
-          <Tab>Title 2</Tab>
+          <Tab onClick={() => setCategory('')}>All Jobs</Tab>
+          <Tab onClick={() => setCategory('web development')}>
+            Web Development
+          </Tab>
+          <Tab onClick={() => setCategory('graphics design')}>
+            Graphics Design
+          </Tab>
+          <Tab onClick={() => setCategory('digital marketing')}>
+            Digital Marketing
+          </Tab>
         </TabList>
 
         <TabPanel>
-          <Tab>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 container mx-auto">
-              {allJobs.map((job) => (
-                <div
-                  key={job?._id}
-                  className="card bg-base-100 shadow-xl border-gray-300 border"
-                >
-                  <div className="card-body">
-                    <h2 className="card-title">{job?.jobTitle}</h2>
-                    <p>
-                      <span className="font-semibold">Dead Line:</span>{' '}
-                      <span className="text-red-400">{job?.deadline}</span>
-                    </p>
-                    <p>
-                      <span className="font-semibold">Price Range:</span>{' '}
-                      {job?.minimumPrice}$ to {job?.maximumPrice}$
-                    </p>
-                    <p>{job?.description}</p>
-                    <div className="card-actions justify-end">
-                      <button className="btn btn-primary">Bid Now</button>
-                    </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 container mx-auto">
+            {allJobs?.data?.map((job) => (
+              <div
+                key={job?._id}
+                className="card bg-base-100 shadow-xl border-gray-300 border"
+              >
+                <div className="card-body">
+                  <h2 className="card-title">{job?.jobTitle}</h2>
+                  <p>
+                    <span className="font-semibold">Dead Line:</span>{' '}
+                    <span className="text-red-400">{job?.deadline}</span>
+                  </p>
+                  <p>
+                    <span className="font-semibold">Price Range:</span>{' '}
+                    {job?.minimumPrice}$ to {job?.maximumPrice}$
+                  </p>
+                  <p>{job?.description.slice(0, 59)}...</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Bid Now</button>
                   </div>
                 </div>
-              ))}
-            </div>
-          </Tab>
+              </div>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 container mx-auto">
+            {allJobs?.data?.map((job) => (
+              <div
+                key={job?._id}
+                className="card bg-base-100 shadow-xl border-gray-300 border"
+              >
+                <div className="card-body">
+                  <h2 className="card-title">{job?.jobTitle}</h2>
+                  <p>
+                    <span className="font-semibold">Dead Line:</span>{' '}
+                    <span className="text-red-400">{job?.deadline}</span>
+                  </p>
+                  <p>
+                    <span className="font-semibold">Price Range:</span>{' '}
+                    {job?.minimumPrice}$ to {job?.maximumPrice}$
+                  </p>
+                  <p>{job?.description.slice(0, 59)}...</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Bid Now</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 container mx-auto">
+            {allJobs?.data?.map((job) => (
+              <div
+                key={job?._id}
+                className="card bg-base-100 shadow-xl border-gray-300 border"
+              >
+                <div className="card-body">
+                  <h2 className="card-title">{job?.jobTitle}</h2>
+                  <p>
+                    <span className="font-semibold">Dead Line:</span>{' '}
+                    <span className="text-red-400">{job?.deadline}</span>
+                  </p>
+                  <p>
+                    <span className="font-semibold">Price Range:</span>{' '}
+                    {job?.minimumPrice}$ to {job?.maximumPrice}$
+                  </p>
+                  <p>{job?.description.slice(0, 59)}...</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Bid Now</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 container mx-auto">
+            {allJobs?.data?.map((job) => (
+              <div
+                key={job?._id}
+                className="card bg-base-100 shadow-xl border-gray-300 border"
+              >
+                <div className="card-body">
+                  <h2 className="card-title">{job?.jobTitle}</h2>
+                  <p>
+                    <span className="font-semibold">Dead Line:</span>{' '}
+                    <span className="text-red-400">{job?.deadline}</span>
+                  </p>
+                  <p>
+                    <span className="font-semibold">Price Range:</span>{' '}
+                    {job?.minimumPrice}$ to {job?.maximumPrice}$
+                  </p>
+                  <p>{job?.description.slice(0, 59)}...</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Bid Now</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </TabPanel>
       </Tabs>
     </section>
