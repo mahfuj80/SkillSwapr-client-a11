d18c32cf87a943de074aca01964e6fba3a3d54ee
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-08 16:14:30 +0600

    server link change

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -56,7 +56,7 @@ const AuthProvider = ({ children }) => {
       // if user exists then issue a token
       if (currentUser) {
         axios
-          .post('http://localhost:5000/jwt', loggedUser, {
+          .post('https://skill-swapr-server-a11.vercel.app/jwt', loggedUser, {
             withCredentials: true,
           })
           .then((res) => {
@@ -64,9 +64,13 @@ const AuthProvider = ({ children }) => {
           });
       } else {
         axios
-          .post('http://localhost:5000/logout', loggedUser, {
-            withCredentials: true,
-          })
+          .post(
+            'https://skill-swapr-server-a11.vercel.app/logout',
+            loggedUser,
+            {
+              withCredentials: true,
+            }
+          )
           .then((res) => {
             console.log(res.data);
           });
