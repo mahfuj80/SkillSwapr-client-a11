d397082a714683e1af9bd864125100532106e784
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-08 06:23:01 +0600

    dark theme retuch

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import logo from '../../assets/logo.png';
 const Footer = () => {
   return (
-    <footer className="relative bg-white pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+    <footer className="relative bg-white pt-20 pb-10 lg:pt-[120px] lg:pb-20 dark:bg-[#1d232a]">
       <div className="container mx-auto">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 sm:w-2/3 lg:w-3/12">
